78df2e25161a92c93cca06f758b2e49046958264
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-02-22 16:34:14 +0800

    update nice-bar

--- pages/nice-bar/js/bundle.js
+++ pages/nice-bar/js/bundle.js
@@ -474,6 +474,20 @@ function s4() {
 
 },{}]},{},[1]);
 
+(function() {
+  "use strict";
+
+  var str = '<a id="avatar" class="avatar" href="http://forsigner.com/" target="_blank" style="position: absolute;top: 20px;display: inline-block;left: 50%;margin-left:-30px;">' +
+    '<img src="http://forsigner.com/images/avatar.jpg" alt="forsigner" title="go home" style="width: 60px; height: 60px;border-radius: 50%;">' +
+    '</a>';
+
+  var $div = document.createElement('div');
+  $div.innerHTML = str;
+  document.body.appendChild($div);
+
+
+}());
+
 'use strict';
 
 var data = [{
