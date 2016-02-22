cd3eeccdde4c7f1eeec2415d26616f9c89b0f84b
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-02-22 15:15:23 +0800

    update pages

--- pages/util-guid/js/bundle.js
+++ pages/util-guid/js/bundle.js
@@ -44,6 +44,20 @@ module.exports = function (length) {
 
 },{}]},{},[1]);
 
+(function() {
+  "use strict";
+
+  var str = '<a id="avatar" class="avatar" href="http://forsigner.com/" target="_blank" style="position: absolute;top: 20px;display: inline-block;left: 50%;margin-left:-30px;">' +
+    '<img src="http://forsigner.com/images/avatar.jpg" alt="forsigner" title="go home" style="width: 60px; height: 60px;border-radius: 50%;">' +
+    '</a>';
+
+  var $div = document.createElement('div');
+  $div.innerHTML = str;
+  document.body.appendChild($div);
+
+
+}());
+
 'use strict';
 
 $(function () {
