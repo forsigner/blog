b5a0a2587b68d436b80ad801529f1f199f7213ae
forsigner <forsigner@gmail.com>
2018-10-01 01:27:16 +0800

    update stamen site

---
+++ docs/stamen/en/doczrc.js
@@ -0,0 +1,13 @@
+export default {
+  title: 'Stamen',
+  description: '👙A sexy state container for React',
+  repository: 'https://github.com/forsigner/stamen',
+  hashRouter: true,
+  base: '/stamen/',
+  dest: '../../../pages/stamen',
+  themeConfig: {
+    colors: {
+      primary: 'tomato',
+    },
+  }
+}
